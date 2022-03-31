add9b7c761df816033761b78bac091ea81787cf0
Andrey-Korchmarek <andrey.korchmarek@yandex.ru>
2022-03-31 17:22:31 +0300

    initial commit

---
+++ 02_HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //println("Hello World!")
+    print("Здравствуйте, ")
+    println("меня зовут Андрей Корчмарек.")
+    print("Я живу в Королёве, Московская область.\n")
+    println("""У меня есть опыт написания программ на С и работы с Doсker
+        в рамках обучения в Programming School 21 от Сбербанка.""".trimMargin())
+    /* Try adding program arguments via Run/Debug configuration.
+     Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+    */
+}
