6cf18d1132081477a0125b56487630865db5c60b
Andrey-Korchmarek <andrey.korchmarek@yandex.ru>
2022-04-24 12:39:44 +0300

    change .gitignore chet sheet: - val - var - try + catch + finally - String? + null - Int + .toInt() - print + println - readLine - .also {} - args - 1 однострочный комментарий

--- cheatsheet/src/main/kotlin/Main.kt
+++ cheatsheet/src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    //println("Hello World!") Это однострочный комментарий
+    val itsValue: Int
+    var itsVariable: String?
+
+    try {
+        itsValue = args[0].toInt()
+        itsVariable = args[2]
+    } catch (e: NumberFormatException) {
+        (-1).also { itsValue = it }
+        itsVariable = "Первым аргументом должно быть целое число!"
+        println(itsVariable)
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        itsValue = 0
+        print("Введите значение переменной - ")
+        itsVariable = readLine()
+        println("А теперь ещё раз:")
+        itsVariable = readLine()
+    } finally {
+        itsVariable = null
+    }
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //println("Program arguments: ${args.joinToString()}")
 }
